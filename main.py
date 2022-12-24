55a24ba3681c73a4d12622be3706306e9983a4c0
David.CPH <38375910+CrazyXDavid@users.noreply.github.com>
2022-12-24 13:15:57 +0800

    update for loop logic

--- main.py
+++ main.py
@@ -3,12 +3,11 @@ import pandas as pd
 
 import talib
 
-#epochs_list = [500, 1000, 1500, 2000]
-#first_layer_param = [50, 100, 150, 200]
-#second_layer_param = [500, 1000, 1500, 2000]
-epochs_list = [5, 10]
-first_layer_param = [500, 100]
-second_layer_param = [50, 100]
+epochs_list = [500, 1000, 1500, 2000]
+
+first_layer_param = [50, 1000, 150, 200, 250]
+second_layer_param = [25, 50, 75, 100, 150]
+
 
 # default df
 df = pd.read_csv("data.csv")
@@ -19,7 +18,6 @@ df.set_index(['open_time'], inplace=True)
 df[f'rsi_7'] = talib.RSI(df.close, timeperiod=7)
 df['macd'], df['macd_signal'], df['macd_hist'] = talib.MACD(df['close'], fastperiod=12, slowperiod=26, signalperiod=9)
 df = df.iloc[33:]
-
 
 X, y = df.drop(columns=['close']), df.close.values
 
@@ -56,75 +54,79 @@ result = []
 
 print("Start training models")
 for e in epochs_list:
-  for i in range(len(first_layer_param)):
-    epochs = e
-    layer1 = first_layer_param[i];
-    layer2 = second_layer_param[i]
+  for flp in first_layer_param:
+    for slp in second_layer_param:
+      epochs = e
+      layer1 = flp;
+      layer2 = slp
 
-    print("============ LAYERS PARAMS============")
-    print(f"Epochs: {epochs}")
-    print(f"Layer 1: Dense with {layer1}")
-    print(f"Layer 2: LSTM with {layer2}")
-    print()
+      if layer1 >= layer2:
+        continue
 
-    model = Sequential()
-    model.add(Dense(layer1))
-    model.add(LSTM(layer2,activation='relu', return_sequences=False, input_shape = (n_steps, n_features)))
-    model.add(Dense(1))
-    #model.summary()
+      print("============ LAYERS PARAMS============")
+      print(f"Epochs: {epochs}")
+      print(f"Layer 1: Dense with {layer1}")
+      print(f"Layer 2: LSTM with {layer2}")
+      print()
 
-    model.compile(optimizer = 'adam', loss = 'mse', metrics=['mse', 'mape'])
-    history = model.fit(X_train, y_train, batch_size=180, epochs=epochs, verbose=1)
+      model = Sequential()
+      model.add(Dense(layer1))
+      model.add(LSTM(layer2,activation='relu', return_sequences=False, input_shape = (n_steps, n_features)))
+      model.add(Dense(1))
+      #model.summary()
 
-    model.save(f'models/epochs{epochs}_{n_steps}_{n_features}_D{layer1}_L{layer2}')
+      model.compile(optimizer = 'adam', loss = 'mse', metrics=['mse', 'mape'])
+      history = model.fit(X_train, y_train, batch_size=180, epochs=epochs, verbose=1)
 
-    import matplotlib.pyplot as plt
-    plt.title('train & loss')
-    plt.ylabel('loss')
-    plt.xlabel('Epoch')
+      model.save(f'models/epochs{epochs}_{n_steps}_{n_features}_D{layer1}_L{layer2}')
 
-    plt.plot(history.history['loss'])
-    print(history.history['loss'])
-    start = len(X)-1830
-    end = len(X)-30
+      import matplotlib.pyplot as plt
+      plt.title('train & loss')
+      plt.ylabel('loss')
+      plt.xlabel('Epoch')
 
-    X_test = X[start:end]
-    predict = model.predict(X_test)
-    predictions = pd.DataFrame(predict).rename(columns={0: 'Predict'})
+      plt.plot(history.history['loss'])
+      print(history.history['loss'])
+      start = len(X)-1830
+      end = len(X)-30
 
-    y_test = y[start:end]
-    y_test = pd.DataFrame(y_test).rename(columns={0: 'Real'})
+      X_test = X[start:end]
+      predict = model.predict(X_test)
+      predictions = pd.DataFrame(predict).rename(columns={0: 'Predict'})
+
+      y_test = y[start:end]
+      y_test = pd.DataFrame(y_test).rename(columns={0: 'Real'})
 
 
-    final = pd.concat([predictions,y_test],axis=1)
-    final['mae'] = abs(final['Predict'] - final['Real'])
-    print(final)
-    print(final['mae'].mean())
+      final = pd.concat([predictions,y_test],axis=1)
+      final['mae'] = abs(final['Predict'] - final['Real'])
+      print(final)
+      print(final['mae'].mean())
 
-    avg_mae_mean = f"{(final['Predict'].mean()-final['Real'].mean()) / final['Real'].mean() * 100}%"
+      avg_mae_mean = f"{(final['Predict'].mean()-final['Real'].mean()) / final['Real'].mean() * 100}%"
 
-    #norm_data = pd.DataFrame(scaler.inverse_transform(norm_data), columns=df.columns, index=df.index)
-    nd = normal_data
-    for i in range(len(predict)):
-      nd.iloc[start+i+1+n]['close'] = predict[i][0]
+      #norm_data = pd.DataFrame(scaler.inverse_transform(norm_data), columns=df.columns, index=df.index)
+      nd = normal_data
+      for i in range(len(predict)):
+        nd.iloc[start+i+1+n]['close'] = predict[i][0]
 
-    nd = pd.DataFrame(scaler.inverse_transform(nd), columns=df.columns, index=df.index)
+      nd = pd.DataFrame(scaler.inverse_transform(nd), columns=df.columns, index=df.index)
 
-    # 預計 - 實際
-    a = pd.concat([(nd['close'][start-30:end+30]).rename("Predict"), (df['close'][start-30:end+30]).rename("Real")],axis=1)
-    nd['close'][start-30:end+30].plot(color='#00ff00') # 預計
-    df['close'][start-30:end+30].plot(color='#FF0000') # 實際
+      # 預計 - 實際
+      a = pd.concat([(nd['close'][start-30:end+30]).rename("Predict"), (df['close'][start-30:end+30]).rename("Real")],axis=1)
+      nd['close'][start-30:end+30].plot(color='#00ff00') # 預計
+      df['close'][start-30:end+30].plot(color='#FF0000') # 實際
 
-    avg_mae2_mean = f"{(a['Predict'].mean() - a['Real'].mean()) / a['Real'].mean() * 100}%"
+      avg_mae2_mean = f"{(a['Predict'].mean() - a['Real'].mean()) / a['Real'].mean() * 100}%"
 
-    a.to_excel(f"xls/epochs{epochs}_{n_steps}_{n_features}_D{layer1}_L{layer2}.xlsx")
-    # a.plot()
-    result.append({
-      'layer1': layer1,
-      'layer2': layer2,
-      'epochs': epochs,
-      'avg. mae mean': avg_mae_mean,
-      'avg. Actual mean': avg_mae2_mean
-    })
+      a.to_excel(f"xls/epochs{epochs}_{n_steps}_{n_features}_D{layer1}_L{layer2}.xlsx")
+      # a.plot()
+      result.append({
+        'layer1': layer1,
+        'layer2': layer2,
+        'epochs': epochs,
+        'avg. mae mean': avg_mae_mean,
+        'avg. Actual mean': avg_mae2_mean
+      })
 
 pd.DataFrame(result).to_excel("xls/result.xlsx")
